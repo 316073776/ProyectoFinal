20910f3ea850546a184c638ada4d3ae55fd9248e
AngelMendezJ <mdzangel28@gmail.com>
2022-05-23 22:54:25 -0500

    formularios y altas de discos, artistas, compositores y productores

--- form_productores.php
+++ form_productores.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Formulario Alta Artistas</title>
+		<title>Formulario Alta Productores</title>
 		<script src="js/jquery-3.6.0.js"></script>
-		<script src="js/ajaxformartistas.js"></script>
+		<script src="js/ajaxformproductores.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
 	</head>
 	<body>
-		<h1>Formulario para registro de artistas</h1>
+		<h1>Formulario para registro de productores</h1>
 		<form id="formulario">
 			<label for="nombre">Nombre: </label>
 			<input type="text" name="nombre" id="nombre">
@@ -16,15 +16,9 @@
 			<label for="apellido">Apellido: </label>
 			<input type="text" name="apellido" id="apellido">
 			<br/>
-			<label for="pais_nacimiento">Pais de nacimiento: </label>
-			<input type="text" name="pais_nacimiento" id="pais_nacimiento">
-			<br/>
 			<label for="fecha_nacimiento">Fecha de nacimiento: </label>
 			<input type="date" name="fecha_nacimiento" id="fecha_nacimiento">
 			<br/>
-			<label for="nombre_artistico">Nombre artistico: </label>
-                        <input type="text" name="nombre_artistico" id="nombre_artistico">
-                        <br/>
 
 			<button type="submit" id="enviar">Confirmar</button>
 		</form>
