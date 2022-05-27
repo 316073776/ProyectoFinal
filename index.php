3b1c8fb2f76c12f05e5c58c4bfb00a4784176f1a
Stephanie_Segura <stephanie_segura_rios@hotmail.com>
2022-05-27 13:00:46 -0500

    Se modifica index

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-<html><p>Hola</p></html>
+<!doctype html>
+<html lang="en">
+
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Proyecto final</title>
+
+    <link href="css/stylelogin.css" rel="stylesheet">
+    </head>
+    
+    <body>
+
+		<div class="container">
+		  <div class="brand-logo"></div>
+		  <div class="brand-title">INICIAR SESIÓN</div>
+		  <div class="inputs">
+
+		  	<form id="login-form" class="form" action="./peticiones/login.php" method="post">
+		    
+		        <label for="username">USUARIO:</label>
+		        <input type="text" name="username" id="username" placeholder="Usuario">
+		        
+				<label for="password">CONTRASEÑA:</label>
+		        <input type="password" name="password" id="password" placeholder="Min 6 caracteres">
+		        
+		        <button type="submit">LOGIN</button>
+		       
+			</form>
+
+		  </div>
+		</div>
+         
+    </body>
+</html>
---
+++ peticiones/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<p>Hola login.php</p>
+<p>verifica, inicia sesion y redirige a <a href="catalogo_discos.php">catálogo_discos.php</a> en caso contrario, regresa a index.php</p>
+</body>
+</html>
+
+<?php  
+	session_start();
+?>
