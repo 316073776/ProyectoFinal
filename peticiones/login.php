0100c0e5a0ae750177adcb34d888d642ecc72113
David Latapi <david211910@gmail.com>
2022-05-28 12:13:42 -0500

    Login

--- peticiones/login.php
+++ peticiones/login.php
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-</head>
-<body>
-<p>Hola login.php</p>
-<p>verifica, inicia sesion y redirige a <a href="../catalogo_discos.php">catálogo_discos.php</a> en caso contrario, regresa a index.php</p>
-</body>
-</html>
-=======
 <?php
 	require_once '../conexion.php';
 	$username = $_POST['username'];
 	$contrasena = $_POST['contrasena'];
->>>>>>> 1aae23a79ab97b47609ae189762b6143615d8dee
 
 	if(preg_match('/[A-Za-z áéíóúñ]{2,50}/i', $username)){
         	if(preg_match('/[A-Za-z áéíóúñ]{2,50}/i', $contrasena)){
