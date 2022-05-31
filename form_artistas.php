443c6f2389fbe59382e7ed4defbc4cf0bf8b276a
AngelMendezJ <mdzangel28@gmail.com>
2022-05-30 21:20:11 -0500

    correccion

--- form_artistas.php
+++ form_artistas.php
@@ -8,7 +8,7 @@
 	<body>
 
 		<?php require_once 'templates/menu.php'; ?>
-		<div class="flex flex-col justify-center items-center mt-5 mb-5">
+		 <div class="flex flex-col justify-center items-center mt-5 mb-5">
 			<div class="rounded shadow-lg overflow-hidden w-6/12 flex flex-col p-5">
 				<h1 class="uppercase text-indigo-600 font-bold text-4xl mb-5">Formulario para registro de artistas</h1>
 				<form id="formulario">
