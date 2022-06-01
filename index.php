a52b0133385010ee9d8244bff2a5d9624b467f28
Segura Rios Brenda Stephanie <stephanie_Segura_rios@hotmail.com>
2022-06-01 17:30:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,28 +4,29 @@
 		header("Location: catalogo_discos.php");
 	}
 ?>
+
 <?php require_once 'templates/header.php'; ?>
-    <body class="login">
 
-		<div class="container-login">
-		  <div class="brand-logo"></div>
-		  <div class="brand-title">INICIAR SESIÓN</div>
-		  <div class="inputs">
-
-		  	<form id="login-form" class="form" action="./peticiones/login.php" method="post">
+<body class="login">
+	<div class="container-login">
+		<div class="brand-logo"></div>
+		<div class="brand-title">INICIAR SESIÓN</div>
+		
+		<div class="inputs">
+			<form id="login-form" class="form" action="./peticiones/login.php" method="post">
 		    
-		        <label for="username">USUARIO:</label>
-		        <input type="text" name="username" id="username" placeholder="Usuario">
-		        
+				<label for="username">USUARIO:</label>
+				<input type="text" name="username" id="username" placeholder="Usuario" autocomplete="off">
+
 				<label for="contrasena">CONTRASEÑA:</label>
-		        <input type="password" name="contrasena" id="contrasena" placeholder="Min 6 caracteres">
-		        
-		        <button type="submit">LOGIN</button>
+				<input type="password" name="contrasena" id="contrasena" placeholder="Min 8 caracteres">
+
+				<button type="submit">INGRESAR</button>
 		       
 			</form>
-		  </div>
-
-		  <a class="button-reg" href="./form_usuarios.php">Registrate aquí</a>
-		  <a class="credits" href="creditos.php">CRÉDITOS</a>
 		</div>
+
+		<a class="mt-5" href="./form_usuarios.php">Registrate aquí</a>
+		<a class="credits" href="creditos.php">CRÉDITOS</a>
+	</div>
 <?php require_once 'templates/footer.php'; ?>>
