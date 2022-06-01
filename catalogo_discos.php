5c736f2be389c9869b11598c648d5b61a626d68c
AngelMendezJ <mdzangel28@gmail.com>
2022-05-31 21:39:04 -0500

    Conexion a bd catalogo

--- catalogo_discos.php
+++ catalogo_discos.php
@@ -8,7 +8,11 @@
 ?>
 
   <body>
-
+    <?php
+                include 'conexion.php';
+                $query = "Select disco_id, titulo, año, genero, costo from bdrecords.discos";
+                $ejecucion = pg_query($con,$query);
+    ?>
     <?php require_once 'templates/menu.php'; ?>
       <div class="flex flex-col justify-center items-center mt-5 mb-5">
         <div class="rounded shadow-lg overflow-hidden w-6/12 flex flex-col p-4">
@@ -29,10 +33,23 @@
             <tbody>
               <!--Hacer un ciclo for para la tabla-->
               <tr>
-                <td class="items-center">disco 1aaaaaaaaaaaaaaaaaaaaaaaa</td>
-                <td class="items-center">grupo 1aaaaaaaaaaaaaaaaaaaaaaa</td>
-                <td class="items-center" style="cursor: pointer;"><i class="bi bi-pencil-square"></i></td>
-                <td class="items-center" style="cursor: pointer;"><i class="bi bi-trash-fill"></i></td>
+		<?php
+
+                	while($row = pg_fetch_assoc($ejecucion)){
+                        	echo "<tr>";
+                                echo "<td>".$row['disco_id']."</td>";
+                                echo "<td>".$row['titulo']."</td>";
+                                echo "<td>".$row['año']."</td>";
+                                echo "<td>".$row['genero']."</td>";
+                                echo "<td>".$row['costo']."</td>";
+		?>
+				<td class="items-center" style="cursor: pointer;"><i class="bi bi-pencil-square"></i></td>
+                		<td class="items-center" style="cursor: pointer;"><i class="bi bi-trash-fill"></i></td>
+                                        <?php       
+					 echo "</tr>";
+
+                                                        }
+                                        ?>
               </tr>
             </tbody>
           </table>
