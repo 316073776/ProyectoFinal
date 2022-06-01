8fbd3f8a2c2403dd2a3372d7ac92feb4f7a6a49e
David Latapi <david211910@gmail.com>
2022-06-01 11:48:06 -0500

    Eliminar disco

---
+++ eliminar_disco.php
@@ -0,0 +1,20 @@
+<?php 
+	if(!isUser()) header('Location: index.php');
+
+    if(isset($_GET['disco_id'])){
+
+        $id = $_GET['disco_id'];
+        $sql = "DELETE FROM bdrecords.discos WHERE disco_id = $id;";
+
+        $result = pg_query($con, $sql);
+
+        header('Location: catalogo_discos.php');
+            
+        
+
+    }else{
+
+        header('Location: catalogo_discos.php');
+    }
+
+?>
