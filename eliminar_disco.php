504f6d6ff15341fb63289584e943a2848e934991
root <root@debianVM.davidMiller.com>
2022-06-01 12:05:29 -0500

    Eliminar

--- eliminar_disco.php
+++ eliminar_disco.php
@@ -6,6 +6,8 @@
     if(isset($_GET['disco_id'])){
 
         $id = $_GET['disco_id'];
+	$sql = "DELETE FROM bdrecords.disco_cancion WHERE disco_id = $id;";
+	$result = pg_query($con, $sql);
         $sql = "DELETE FROM bdrecords.discos WHERE disco_id = $id;";
 
         $result = pg_query($con, $sql);
