d006df9e149e2389afabc7360b1832c39b689587
Stephanie_Segura <stephanie_segura_rios@hotmail.com>
2022-06-01 18:06:02 -0500

    Cambiando estilos

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
 <?php require_once 'templates/header.php'; ?>
 
 <body class="login">
+
+	<script src="./js/jquery-3.6.0.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.4/jquery.validate.min.js" integrity="sha512-FOhq9HThdn7ltbK8abmGn60A/EMtEzIzv1rvuh+DqzJtSGq8BRdEN0U+j0iKEIffiw/yEtVuladk6rsG4X6Uqg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<script src="./js/ajaxformlogin.js"></script>	
+
 	<div class="container-login">
 		<div class="brand-logo"></div>
 		<div class="brand-title">INICIAR SESIÓN</div>
@@ -29,4 +34,5 @@
 		<a class="mt-5" href="./form_usuarios.php">Registrate aquí</a>
 		<a class="credits" href="creditos.php">CRÉDITOS</a>
 	</div>
-<?php require_once 'templates/footer.php'; ?>>
+</body>
+</html>
