9be0598a30f0a8fc51039aa03497d521102f8200
Stephanie_Segura <stephanie_segura_rios@hotmail.com>
2022-06-01 20:21:51 -0500

    Cambiando estilos

--- templates/menu.php
+++ templates/menu.php
@@ -1,14 +1,17 @@
-<ul class="nav">
-			<li tabindex="0" class="active"><i class="bi bi-house-fill"></i><a href="../catalogo_discos.php">Inicio</a></li>
-			<li tabindex="0"><i class="bi bi-people-fill"></i><a href="../creditos.php">Créditos</a></li>
-			<?php if(isset($_SESSION) && isset($_SESSION['usuario'])): ?>
-				<li tabindex="0"><a href="../catalogo_artistas.php">Artistas</a></li>
-				<li tabindex="0"><a href="../catalogo_compositores.php">Compositores</a></li>
-				<li tabindex="0"><a href="../catalogo_discos.php">Discos</a></li>
-				<li tabindex="0"></i><a href="../catalogo_disqueras.php">Disqueras</a></li>
-				<li tabindex="0"><a href="../catalogo_grupos.php">Grupos</a></li>
-				<li tabindex="0"><a href="../catalogo_productores.php">Productores</a></li>
-				<li tabindex="0"><a href="../catalogo_canciones.php">Canciones</a></li>
-				<li tabindex="0"><i class="bi bi-door-open-fill"></i><a href="../logout.php">Cerrar sesión</a></li>
-			<?php endif; ?>
-</ul>
+<?php if(isset($_SESSION) && isset($_SESSION['usuario'])): ?>
+	<ul class="nav">
+			
+		<li tabindex="0"><a href="../catalogo_artistas.php">Artistas</a></li>
+		<li tabindex="0"><a href="../catalogo_compositores.php">Compositores</a></li>
+		<li tabindex="0"><a href="../catalogo_discos.php">Discos</a></li>
+		<li tabindex="0"></i><a href="../catalogo_disqueras.php">Disqueras</a></li>
+		<li tabindex="0"><a href="../catalogo_grupos.php">Grupos</a></li>
+		<li tabindex="0"><a href="../catalogo_productores.php">Productores</a></li>
+		<li tabindex="0"><a href="../catalogo_canciones.php">Canciones</a></li>
+		<li tabindex="0"><i class="bi bi-people-fill"></i><a href="../creditos.php">Créditos</a></li>
+		<li tabindex="0" class="active"><i class="bi bi-house-fill"></i><a href="../catalogo_discos.php">Inicio</a></li>
+		<li tabindex="0"><i class="bi bi-door-open-fill"></i><a href="../logout.php">Cerrar sesión</a></li>
+
+	</ul>
+<?php endif; ?>
+
